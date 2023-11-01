1901357f4186d65aa0ca8ba61befc70e21d68a36
Ebod Shojaei <ebod.shojaei@gmail.com>
2023-11-01 02:00:50 -0700

    Added function to read in text and tokenize each string.

--- a2.c
+++ a2.c
@@ -35,16 +35,16 @@
 // [3] Allow for sorting by all students.
 
 typedef struct Student {
-	char[] first_name; // Alphabet
-	char[] last_name; // Alphabet
-	char[] birth_month; // Ranges from Jan to Dec
-	char[] birth_day; // Ranges from 1 to 31
-	char[] birth_year; // Ranges from 1950 to 2010
-	char[] gpa; // Ranges from 0.0 to 4.3
-	char[] student_status; // Either Domestic (D) or International (I)
-	char[] toefl; // Ranges from 0 to 120
+	char *first_name; // Alphabet
+	char *last_name; // Alphabet
+	char *birth_month; // Ranges from Jan to Dec
+	char *birth_day; // Ranges from 1 to 31
+	char *birth_year; // Ranges from 1950 to 2010
+	char *gpa; // Ranges from 0.0 to 4.3
+	char *student_status; // Either Domestic (D) or International (I)
+	char *toefl; // Ranges from 0 to 120
 
-	Student *next;
+	struct Student *next;
 } Student_t;
 
 
@@ -55,26 +55,41 @@ typedef struct Student {
 // Condition to sort by last name - Assume everyone has a last name. Output case same as input.
 // Condition to sort by first name - Assume everyone has a first name. Output case same as input.
 // Condition to sort by GPA - Assume everyone has a GPA up to 3 decimal places
-// Condition to sort by TOEFL - Assume every
+// Condition to sort by TOEFL
 // Condition to sort by student_status
 // Function to merge sort
 // Function to read
 
-// Function to check if name
-// Function to check if bi
+// Function to check if first or last name data (if not first/second str, then invalid format)
+// Function to check if birth data (if <1950 or >2010, then invalid format)
+// Function to check if GPA data (if >3 decimal places, then invalid format)
+// Function to check if status data (
+// Function to check if TOEFL data (if domestic student, then invalid format)
+// 
+// There are 6 strings per line.
 
 /**
  * Read in text. Checks conditions meeting fields.
  */ 
-void readFile(FILE *file, Student_t *node) {
+void readFile(FILE *file) {
 	// Use strtok for next string of file stream
 	// Is this a name?
-	char[] *line = (char *) malloc( * sizeof(char));
-	while (fgets(line, 256, file) != EOF) {
-		char[] *word = strtok(line, " ");
-	
+	char *line = (char *) malloc(256 * sizeof(char));
+	while (fgets(line, 256, file)) {
+		int counter = 0; // Reset on new line. Up to 6 inputs
+		char *delimiter = " ";
+		char *word = strtok(line, delimiter); // Gets the first string
+		
+		while (word != NULL) {
+			printf("%s\n", word);
+			counter++;
+			if (counter > 6) {
+				printf("Error: Invalid input format.\n");
+				exit(1); // Bad input. Terminate program.
+			}
+			word = strtok(NULL, delimiter); // Gets the next string
+		}
 	}
-	
 }
 
 
@@ -86,9 +101,9 @@ void readFile(FILE *file, Student_t *node) {
  * Option is integer of 1, 2, or 3.
  *
  */ 
-int main(int argc, char argc[]) {
+int main(int argc, char *argv[]) {
 	if (argc != 4) {
-		printf("Error: Usage %s <input_file> <output_file> <option>", argv[0]);
+		printf("Error: Usage %s <input_file> <output_file> <option>\n", argv[0]);
 		return 1;
 	}
 
@@ -96,9 +111,11 @@ int main(int argc, char argc[]) {
 	FILE *file = fopen(input_name, "r");
 
 	if (file == NULL) {
-		printf("Error: File not found."\n);
+		printf("Error: File not found.\n");
 		return 1;
 	}
+
+	readFile(file);
 
 	return 0;
 }
